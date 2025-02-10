77483a5024bd90905d3cc4263a334d915089d534
Enrico Fraccaroli (Galfurian) <enry.frak@gmail.com>
2025-02-10 12:14:36 -0500

    Update version.

--- include/ustr/version.hpp
+++ include/ustr/version.hpp
@@ -9,6 +9,6 @@
 
 enum : unsigned char {
     USTR_MAJOR_VERSION = 1, ///< Major version of the library.
-    USTR_MINOR_VERSION = 0, ///< Minor version of the library.
+    USTR_MINOR_VERSION = 1, ///< Minor version of the library.
     USTR_MICRO_VERSION = 0  ///< Micro version of the library.
 };
