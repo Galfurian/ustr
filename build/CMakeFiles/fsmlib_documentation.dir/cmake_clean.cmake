4bb5eb0a106d103a5bf1d1dcf77cc209bd2d6f7f
Enrico Fraccaroli (Galfurian) <enry.frak@gmail.com>
2025-02-10 13:53:16 -0500

    Update gh-pages.

--- build/CMakeFiles/fsmlib_documentation.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/fsmlib_documentation"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/fsmlib_documentation.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
